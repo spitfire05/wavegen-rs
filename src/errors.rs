8580548e31b14c4bacf964e0fcddc9e4259cb04d
Michal Borejszo <michael.borejszo@gmail.com>
2022-12-26 12:02:32 +0100

    remove thiserror

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,13 @@
-use thiserror::Error;
+use core::fmt::Display;
 
-#[derive(Error, Debug, Clone, Copy)]
-#[error("Invalid SamplingRate value: `{0}`. SamplingRate has to be positive, non-zero and finite.")]
-/// Error raised when cosntructing [`SamplingRate`] of invalid value.
+#[derive(Debug, Clone, Copy)]
+/// Error raised when cosntructing [`Waveform`] with sample rate of invalid value.
 pub struct InvalidSampleRate(pub(crate) f64);
+
+impl Display for InvalidSampleRate {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "Invalid SamplingRate value: `{}`. SamplingRate has to be positive, non-zero and finite.", self.0)
+    }
+}
+
+impl std::error::Error for InvalidSampleRate {}
