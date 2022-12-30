5d8ac783769527cfa9e72c8e2b64c14159711f91
Michal Borejszo <michael.borejszo@gmail.com>
2022-12-30 13:13:10 +0100

    spectrum example using rustfft

--- examples/spectrum.rs
+++ examples/spectrum.rs
@@ -1,27 +1,39 @@
-use std::path::Path;
-
-use libm::sqrtf;
 use plotters::prelude::*;
-use spectrum_analyzer::{samples_fft_to_spectrum, FrequencyLimit};
-use wavegen::{sine, Waveform};
+use rustfft::{num_complex::Complex, FftPlanner};
+use std::path::Path;
+use wavegen::{sine, square, wf};
+
+const F_MAX: f32 = 350.0;
+const SAMPLE_RATE: f32 = F_MAX * 2.56;
+const N_SAMPLES: usize = 2048;
+const SPECTRUM_RESOLUTION: f32 = SAMPLE_RATE / N_SAMPLES as f32;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let sample_rate = 2560.0;
-    let n = 2048;
+    // Define the waveform to perform FFT on
+    let wf = wf!(
+        f32,
+        SAMPLE_RATE,
+        sine!(300., 10.),
+        sine!(50., 2.),
+        square!(100., 5.)
+    );
+    let samples = wf.into_iter().take(N_SAMPLES);
 
-    let samples = Waveform::<f32>::with_components(sample_rate, vec![sine!(300, 10), sine!(50, 2)])
-        .into_iter()
-        .take(n)
-        .collect::<Vec<f32>>();
+    // Perform FFT
+    let mut planner = FftPlanner::<f32>::new();
+    let fft = planner.plan_fft_forward(N_SAMPLES);
+    let mut buffer: Vec<_> = samples.into_iter().map(|s| Complex::new(s, 0.)).collect();
+    fft.process(&mut buffer);
 
-    let spectrum = samples_fft_to_spectrum(
-            &samples,
-            sample_rate as u32,
-            FrequencyLimit::All,
-            None,
-        ).unwrap();
-
-    draw("spectrum.png", "Spectrum", spectrum.data().iter().map(|(f, a)| (f.val(), a.val() * (1.0 / sqrtf(n as f32)))))?;
+    draw(
+        "spectrum.png",
+        "Spectrum",
+        buffer
+            .into_iter()
+            .enumerate()
+            .map(|(i, c)| (i as f32 * SPECTRUM_RESOLUTION, c.norm() / N_SAMPLES as f32))
+            .take_while(|(f, _)| *f < F_MAX),
+    )?;
 
     Ok(())
 }
@@ -39,19 +51,19 @@ fn draw<I: IntoIterator<Item = (f32, f32)>, P: AsRef<Path>>(
         .margin(5)
         .x_label_area_size(30)
         .y_label_area_size(30)
-        .build_cartesian_2d(-0f32..1024f32, -0f32..350f32)?;
+        .build_cartesian_2d(-0f32..F_MAX, -0f32..10f32)?;
 
     chart.configure_mesh().draw()?;
 
     chart
-        .draw_series(LineSeries::new(iter, &RED))?
+        .draw_series(LineSeries::new(iter, RED))?
         .label(label)
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], RED));
 
     chart
         .configure_series_labels()
-        .background_style(&WHITE.mix(0.8))
-        .border_style(&BLACK)
+        .background_style(WHITE.mix(0.8))
+        .border_style(BLACK)
         .draw()?;
 
     Ok(())
